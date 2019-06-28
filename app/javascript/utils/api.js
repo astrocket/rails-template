d2d95cbed946b1b07a40192b0cfb4158b1e59710
astrocket <leonardo.lee1993@gmail.com>
2019-06-28 22:21:17 +0900

    add put action for api.js

--- app/javascript/utils/api.js
+++ app/javascript/utils/api.js
@@ -30,7 +30,23 @@ const post = (path, params, headers = null) => {
     })
 };
 
+const put = (path, params, headers = null) => {
+  headers = headers || {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+  };
+  headers['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  return axios({
+      method: 'put',
+      url: path,
+      headers: headers,
+      data: params
+  })
+};
+
 export default {
     get: get,
-    post: post
+    post: post,
+    put: put
 }
