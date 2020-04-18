82c3a24df505b7da7dceb189bb15c991b9a0b966
astrocket <leonardo.lee1993@gmail.com>
2020-04-18 14:34:14 +0900

    fix minor text

--- template.rb
+++ template.rb
@@ -202,7 +202,6 @@ after_bundle do
   copy_file "public/robots.txt", force: true
   template "README.md.tt", "README.md", force: true
   git_commit("project ready")
-  puts set_color full_liner("Start by running 'cd #{app_name} && yarn && rails hot'"), :green
+  puts set_color full_liner("Start by running 'cd #{Rails.root.to_s.split("/").last} && yarn && rails hot'"), :green
   puts set_color full_liner(""), :green
 end
-
