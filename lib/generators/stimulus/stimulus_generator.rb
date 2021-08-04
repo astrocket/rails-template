4dea4241a8830127cb8598fb4d2779275955a9fb
astrocket <astrocket.dev@gmail.com>
2021-08-04 11:37:29 +0900

    update stimulus generator

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -1,21 +1,30 @@
 class StimulusGenerator < Rails::Generators::Base
-  source_root File.expand_path('templates', __dir__)
-  argument :controller_name, type: :string, default: false
+  source_root File.expand_path("templates", __dir__)
+  argument :controller_name, type: :string, required: true
   argument :action_name, type: :string, default: false
   attr_accessor :stimulus_path, :controller_pattern
 
   def set_up
     if action_name.present?
-      @stimulus_path = "app/javascript/controllers/#{controller_name}/#{action_name}_controller.js"
-      @controller_pattern = "#{controller_name.split('/').join('--')}--#{action_name.gsub("_", "-")}"
+      @stimulus_path = "app/javascript/controllers/#{controller_name.underscore}/#{action_name.underscore}_controller.js"
+      @controller_pattern = "#{ruby_to_stimulus(controller_name)}--#{ruby_to_stimulus(action_name)}"
     else
-      @stimulus_path = "app/javascript/controllers/#{controller_name}_controller.js"
-      @controller_pattern = "#{controller_name.split('/').join('--')}"
+      @stimulus_path = "app/javascript/controllers/#{controller_name.underscore}_controller.js"
+      @controller_pattern = ruby_to_stimulus(controller_name)
     end
   end
 
   def generate_stimulus
-    template 'controller.js.erb', stimulus_path
+    template "controller.js.erb", stimulus_path
+  end
+
+  private
+
+  def ruby_to_stimulus(string)
+    string.underscore
+      .tr("_", "-")
+      .split("/")
+      .join("--")
   end
 
 end
